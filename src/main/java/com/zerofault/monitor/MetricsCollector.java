a494597afdaf156a49b77eed0deb9d3f90c96304
TaraKingston <tara.kingston05@gmail.com>
2026-02-06 23:36:21 +0000

    update the MetricsCollector class so that the variable can store previous counters.

--- src/main/java/com/zerofault/monitor/MetricsCollector.java
+++ src/main/java/com/zerofault/monitor/MetricsCollector.java
@@ -3,18 +3,30 @@ package com.zerofault.monitor;
 import oshi.SystemInfo;
 import oshi.hardware.CentralProcessor;
 import oshi.hardware.GlobalMemory;
+import oshi.software.os.OperatingSystem;
+import oshi.hardware.NetworkIF;
+import java.util.List;
 
 public class MetricsCollector {
 
     private final CentralProcessor processor;
     private final GlobalMemory memory;
     private long[] prevTicks;
+    private final OperatingSystem os;     // stores gives disk + processes
+    private final List<NetworkIF> nifs;   // stores list of network adapters
+
+    // prev counters so we can compute KB/s rates
+    private long prevRXBytes = -1;
+    private long prevTXBytes = -1;
+    private long prevNETTimeMs = -1;
 
     public MetricsCollector() {
         SystemInfo si = new SystemInfo();
         processor = si.getHardware().getProcessor();
         memory = si.getHardware().getMemory();
         prevTicks = processor.getSystemCpuLoadTicks();
+        os = si.getOperatingSystem();
+        nifs = si.getHardware().getNetworkIFs();
     }
 
     public double getCpuUsage() {
